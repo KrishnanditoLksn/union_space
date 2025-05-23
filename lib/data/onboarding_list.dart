be6e41ec4bf350b191612235351c18fa54c7220c
KrishnanditoLksn <krislaksana123@gmail.com>
2025-05-23 21:57:47 +0700

    feat:image and onboarding list

---
+++ lib/data/onboarding_list.dart
@@ -0,0 +1,24 @@
+import 'model/onboarding.dart';
+
+class OnboardingList {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+      title: "Terhubung.Berkembang.Bersama",
+      description:
+          "Mewakili semangan kolaborasi dan pertumbuhan komunias profesional di Indonesia",
+      image: "assets/image 1.png",
+    ),
+    OnboardingInfo(
+      title: "Ruangmu.Gayamu.Bisnismu.",
+      description:
+          "Menekankan fleksibilitas dan personalisasi yang ditawarkan oleh UnionSpace",
+      image: "assets/image 6.png",
+    ),
+    OnboardingInfo(
+      title: "Mulai Hari Produktifmu Sekarang",
+      description:
+          "Pendek,positif,dan langsung mengajak pengguna untuk mulai beraktifitas.Daftar dan Jelajahi Space Sekarang!",
+      image: "assets/image 7.png",
+    ),
+  ];
+}
